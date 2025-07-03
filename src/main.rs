7d0416889bcbac23001d51298ad19e1d523b3a3f
Ezri Zhu <me@ezrizhu.com>
2025-07-03 18:34:07 -0400

    h

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+use esp_idf_hal::peripherals::Peripherals;
+mod led;
+use led::RGB8;
+use led::WS2812RMT;
 
-    // Bind the log crate to the ESP Logging facilities
+use esp32_nimble::{BLEDevice, BLEScan};
+
+fn main() -> anyhow::Result<()> {
+    esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+
+    let peripherals = Peripherals::take()?;
+    let mut led = WS2812RMT::new(peripherals.pins.gpio8, peripherals.rmt.channel0)?;
+    led.set_pixel(RGB8::new(128, 0, 128))?;
+
+    let ble_device = BLEDevice::take();
+    let ble_scan = BLEScan::new();
+    ble_scan.start(ble_device, 10000, |device, data| {
+        log::info!("{:?}", data.name());
+    });
+    loop {}
 }
