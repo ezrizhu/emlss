d5c4cee8339708011a35b2fd1aa7b7f8783f74e5
Ezri Zhu <me@ezrizhu.com>
2025-07-03 22:19:48 -0400

    woof

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_svc::hal::task::block_on;
 mod led;
 use led::RGB8;
 use led::WS2812RMT;
 
-use esp32_nimble::{BLEDevice, BLEScan};
+//use esp32_nimble::{BLEDevice, BLEScan};
 
 fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
@@ -15,10 +16,24 @@ fn main() -> anyhow::Result<()> {
     let mut led = WS2812RMT::new(peripherals.pins.gpio8, peripherals.rmt.channel0)?;
     led.set_pixel(RGB8::new(128, 0, 128))?;
 
-    let ble_device = BLEDevice::take();
-    let ble_scan = BLEScan::new();
-    ble_scan.start(ble_device, 10000, |device, data| {
-        log::info!("{:?}", data.name());
-    });
-    loop {}
+    Ok(())
+
+    //let ble_device = BLEDevice::take();
+    //let mut ble_scan = BLEScan::new();
+    //block_on(async {
+    //    let ble_device = BLEDevice::take();
+    //    let mut ble_scan = BLEScan::new();
+    //    ble_scan.active_scan(true).interval(100).window(99);
+
+    //    ble_scan
+    //        .start(ble_device, 5000, |device, data| {
+    //            log::info!("Advertised Device: ({:?}, {:?})", device, data);
+    //            None::<()>
+    //        })
+    //        .await?;
+
+    //    log::info!("Scan end");
+
+    //    Ok(())
+    //})
 }
