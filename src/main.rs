a41e4d772eb78ed7a78c0e1a5212e99fa48db676
Ezri Zhu <me@ezrizhu.com>
2025-07-06 18:43:23 -0400

    grr

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,55 @@
 use esp_idf_hal::peripherals::Peripherals;
-use esp_idf_svc::hal::task::block_on;
+use esp_idf_hal::gpio::AnyIOPin;
+use esp_idf_hal::prelude::*;
+use esp_idf_hal::uart;
+use embassy_time::Delay;
+use embassy_executor::Spawner;
+
 mod led;
 use led::RGB8;
 use led::WS2812RMT;
 
+use sds011::SDS011;
+
 //use esp32_nimble::{BLEDevice, BLEScan};
 
-fn main() -> anyhow::Result<()> {
+#[embassy_executor::main]
+async fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
 
     let peripherals = Peripherals::take()?;
-    let mut led = WS2812RMT::new(peripherals.pins.gpio8, peripherals.rmt.channel0)?;
+    let pins = peripherals.pins;
+
+    //led
+    let mut led = WS2812RMT::new(pins.gpio8, peripherals.rmt.channel0)?;
     led.set_pixel(RGB8::new(128, 0, 128))?;
+
+    // sds011
+    let mut uart_event_config = uart::config::EventConfig::default();
+    uart_event_config.rx_fifo_full = Some(10);
+
+    let mut uart_config = uart::config::Config::default().baudrate(Hertz(9_600));
+    uart_config.event_config = uart_event_config;
+
+    let mut uart = uart::AsyncUartDriver::new(
+        peripherals.uart1,
+        pins.gpio1,
+        pins.gpio3,
+        Option::<AnyIOPin>::None,
+        Option::<AnyIOPin>::None,
+        &uart_config,
+    )
+    .unwrap();
+
+    let sds011 = SDS011::new(&mut uart, sds011::Config::default());
+    let mut sds011 = sds011.init(&mut Delay).await.unwrap();
+
+    let dust = sds011.measure(&mut Delay).await.unwrap();
+    log::info!("{}", dust);
+
 
     Ok(())
 
